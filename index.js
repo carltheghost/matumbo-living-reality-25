e975783a93ec5de78e4a652f5e8487575f324c45
Tumbo <33678436+carltheghost@users.noreply.github.com>
2026-09-22 04:14:17 -0400

    Reality .25: add index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+export {
+  NUCLEUS,
+  REALITY_DIRECTIONS,
+  REALITY_BY_ID,
+  vectorKey,
+  vectorDistance,
+  normalizeDirection,
+  directionFor,
+  classifyVector,
+  nearbyPrimaryDirections,
+  createPrimaryAddress,
+  validateLattice,
+} from './reality-lattice.js';
+
+export {
+  RealityNode,
+  RealityEdge,
+  RealityField,
+} from './reality-field.js';
+
+export { createRealityController } from './reality-controller.js';
+export { buildRealityFieldScene } from './reality-field-scene.js';
